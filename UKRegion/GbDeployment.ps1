6f838af7b23e96812efde422837b07de4a368660
ritcs <104420428+ritcs@users.noreply.github.com>
2024-04-15 12:34:12 +0100

    Add files via upload

---
+++ UKRegion/GbDeployment.ps1
@@ -0,0 +1,25 @@
+﻿# Script to define regional settings on Azure Virtual Machines deployed from the market place
+
+#variables
+$regionalsettingsURL = "https://raw.githubusercontent.com/Resolution-IT/AzureAutomation/main/UKRegion/GBRegion.xml"
+$RegionalSettings = "C:\UKRegion.xml"
+
+
+#downdload regional settings file
+$webclient = New-Object System.Net.WebClient
+$webclient.DownloadFile($regionalsettingsURL,$RegionalSettings)
+
+
+# Set Locale, language etc. 
+& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$RegionalSettings`""
+
+# Set languages/culture. Not needed perse.
+Set-WinSystemLocale en-GB
+Set-WinUserLanguageList -LanguageList en-GB -Force
+Set-Culture -CultureInfo en-GB
+Set-WinHomeLocation -GeoId 242
+Set-TimeZone -Name "GMT Standard Time"
+
+# restart virtual machine to apply regional settings to current user. You could also do a logoff and login.
+Start-sleep -Seconds 40
+Restart-Computer
