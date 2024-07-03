b5ed54629ba9d80003b519d36245373d1849aa79
ritcs <104420428+ritcs@users.noreply.github.com>
2024-07-03 08:53:09 +0100

    Update InstallOneDrive.ps1

--- AVDOptimisations/InstallOneDrive.ps1
+++ AVDOptimisations/InstallOneDrive.ps1
@@ -20,6 +20,14 @@ function Log-Message {
 
 # Stage 1: Create directory
 $directoryPath = "C:\Resolution\OneDrive"
+# Check if the directory already exists
+if (-not (Test-Path $directoryPath)) {
+    Write-Output "Directory does not exist. Creating directory..."
+    New-Item -Path $directoryPath -ItemType Directory
+    Write-Output "Directory created successfully at $directoryPath."
+} else {
+    Write-Output "Directory already exists at $directoryPath."
+}
 
 # Stage 2: Download OneDrive
 
